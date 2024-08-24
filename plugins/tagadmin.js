833809acf9f0db76459d71df1c3e2c79075e955c
by:abdullah <90639434+abdullah5151@users.noreply.github.com>
2024-08-24 18:38:59 +0300

    Update tagadmin.js

--- plugins/tagadmin.js
+++ plugins/tagadmin.js
@@ -1,19 +1,21 @@
 module.exports = {
     name: 'tagadmin',
     async onMessage(msg) {
-        if (msg.body.startsWith(`${config.prefix}tagadmin`)) {
+        if (msg.body.trim().startsWith(`{prefix}tagadmin`)) {
             const chat = await msg.getChat();
             const chatId = chat.id._serialized;
             if (chat.isGroup) {
                 const participants = chat.participants;
-                const messageContent = msg.body.replace(`${config.prefix}tagadmin`, '').trim();
+                const messageContent = msg.body.replace(command, '').trim();
                 let message = '';
                 let mentions = [];
+
                 const admins = participants.filter(participant => participant.isAdmin);
                 if (messageContent) {
                     for (let i = 0; i < admins.length; i++) {
                         const admin = admins[i];
                         const contactId = admin.id._serialized;
+
                         message += `@${admin.id.user}\n`;
                         mentions.push(await msg.client.getContactById(contactId));
                     }
@@ -24,6 +26,7 @@ module.exports = {
                     for (let i = 0; i < admins.length; i++) {
                         const admin = admins[i];
                         const contactId = admin.id._serialized;
+
                         message += `@${admin.id.user}\n`;
                         mentions.push(await msg.client.getContactById(contactId));
                     }
