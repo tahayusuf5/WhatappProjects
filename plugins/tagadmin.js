1177adeec00d9a55b43c72fcb34f5581ae2e0277
by:abdullah <90639434+abdullah5151@users.noreply.github.com>
2024-08-24 18:07:30 +0300

    Update tagadmin.js

--- plugins/tagadmin.js
+++ plugins/tagadmin.js
@@ -1,42 +1,25 @@
 module.exports = {
     name: 'tagadmin',
     async onMessage(msg) {
-        const [command, ...textArray] = msg.body.trim().split('\n');
-        const text = textArray.join('\n').trim();
-        if (command.toLowerCase() === `${prefix}tagadmin`) {
-            if (worktype === 'public') {
-                const chat = await msg.getChat();
-                const chatId = chat.id._serialized;
-                if (chat.isGroup) {
-                    const participants = chat.participants;
-                    let message = '';
-                    let mentions = [];
-                    for (let i = 0; i < participants.length; i++) {
-                        const participant = participants[i];
-                        if (participant.isAdmin) {
-                            const contactId = participant.id._serialized;
-                            message += `• @${participant.id.user}`;
-                            if (i < participants.length - 1) {
-                                message += '\n'; 
-                            }
-                            mentions.push(await msg.client.getContactById(contactId));
-                        }
-                    }
-
-                    if (mentions.length > 0) {
-                        let invisibleText = '⠀'; 
-                        if (text) {
-                            message = `${text}\n\n${message}`; 
-                        }
-                        await msg.client.sendMessage(chatId, invisibleText + message, {
-                            mentions: mentions
-                        });
-                    } else {
-                        await msg.client.sendMessage(chatId, 'Bu grup içinde admin bulunmuyor.');
-                    }
-                } else {
-                    await msg.client.sendMessage(msg.from, 'Bu komutu kullanabilmeniz için grup sohbetinde olmanız gerekmektedir.');
+        if (msg.body.startsWith(`${prefix}tagadmin`)) {
+            const chat = await msg.getChat();
+            const chatId = chat.id._serialized;
+            if (chat.isGroup) {
+                const participants = chat.participants;
+                const messageContent = msg.body.replace(`${config.prefix}tagadmin`, '').trim();
+                let message = '';
+                let mentions = [];
+                for (let i = 0; i < participants.length; i++) {
+                    const participant = participants[i];
+                    const contactId = participant.id._serialized;
+                    message += `@${participant.id.user}\n`;
+                    mentions.push(await msg.client.getContactById(contactId));
                 }
+                await msg.client.sendMessage(chatId, messageContent, {
+                    mentions: mentions
+                });
+            } else {
+                await msg.client.sendMessage(msg.from, 'Bu komut sadece grup sohbetlerinde kullanılabilir.');
             }
         }
     }
